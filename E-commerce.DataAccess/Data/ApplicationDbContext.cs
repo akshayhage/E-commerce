9b4470f9d9ef4df57783b2b70c4cd3f6e3e1244a
Akshay Hage <hageaw10@gmail.com>
2023-06-19 11:32:22 +0530

    Product CRUD successfully completed

--- E-commerce.DataAccess/Data/ApplicationDbContext.cs
+++ E-commerce.DataAccess/Data/ApplicationDbContext.cs
@@ -11,6 +11,7 @@ namespace E_commerce.DataAccess.Data
 
         }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -18,9 +19,72 @@ namespace E_commerce.DataAccess.Data
             new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
             new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
             new Category { Id = 3, Name = "History", DisplayOrder = 3 }
-
-
             );
+
+            modelBuilder.Entity<Product>().HasData(
+
+                new Product
+                {
+                    Id = 1,
+                    Title = "Failure is a Teacher",
+                    Author = "A. P. J. Abdul Kalam\r\n",
+                    Description = "‘Failure will never overtake me if my determination to succeed is strong enough.’ Oftentimes, our desire to succeed doesn’t account for the failure, when in fact, failing at something can teach us the most about how to succeed.",
+                    ISBN = "SWD9999001",
+                    ListPrice = 99,
+                    Price = 90,
+                    Price50 = 85,
+                    Price100 = 80
+                },
+                new Product
+                {
+                    Id = 2,
+                    Title = "The Power of Positive Thinking",
+                    Author = "Norman Vincent Peale",
+                    Description = "It is a practical, direct-action application of spiritual techniques to overcome defeat and win confidence, success, and joy. Norman Vincent Peale, the father of positive thinking and one of the most widely read inspirational writers of all time, shares his famous formula of faith and optimism which millions of people have taken as their own simple and effective philosophy of living.",
+                    ISBN = "CAW777777701",
+                    ListPrice = 40,
+                    Price = 30,
+                    Price50 = 25,
+                    Price100 = 20
+                },
+                new Product
+                {
+                    Id = 3,
+                    Title = "How to Stop Worrying and Start Living",
+                    Author = "Dale Carnegie",
+                    Description = "This book explores the nature of stress and how it infiltrates every level of your life, including the physical, emotional, cognitive, relational, and even spiritual. Through techniques that get to the heart of your unique stress response and an exploration of how stress can affect your relationships, you will discover how to control stress instead of letting it control you.",
+                    ISBN = "RITO5555501",
+                    ListPrice = 55,
+                    Price = 50,
+                    Price50 = 40,
+                    Price100 = 35
+                },
+                new Product
+                {
+                    Id = 4,
+                    Title = "The Magic of Believing",
+                    Author = "Claude M Bristol ",
+                    Description = "This book shows you how you become what you contemplate, why hard work alone will not bring success, how to bring the subconscious into practical action, how to turn your thoughts into achievements, and how belief makes things happen",
+                    ISBN = "WS3333333301",
+                    ListPrice = 70,
+                    Price = 65,
+                    Price50 = 60,
+                    Price100 = 55
+                },
+                new Product
+                {
+                    Id = 5,
+                    Title = "The Power of Your Subconscious Mind",
+                    Author = "Joseph Murphy",
+                    Description = "In this book, the author fuses his spiritual wisdom and scientific research to bring to light how the subconscious mind can be a major influence on our daily lives. Once you understand your subconscious mind, you can also control or get rid of the various phobias that you may have in turn opening a brand new world of positive energy.",
+                    ISBN = "SOTJ1111111101",
+                    ListPrice = 30,
+                    Price = 27,
+                    Price50 = 25,
+                    Price100 = 20
+                }
+              
+                );
         }
     }
 }
---
+++ E-commerceWeb/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,94 @@
+﻿using E_commerce.DataAccess.Repository.IRepository;
+using E_commerce.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_commerceWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ProductController(IUnitOfWork db)
+        {
+            _unitOfWork = db;
+        }
+        public IActionResult Index()
+        {
+            List<Product> products = _unitOfWork.Product.GetAll().ToList();
+            return View(products);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Add(product);
+                _unitOfWork.Save();
+                TempData["success"] = "Product created successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null | id == 0)
+            {
+                return NotFound();
+            }
+            Product productFromDb = _unitOfWork.Product.Get(u => u.Id == id);
+            if (productFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(productFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Product obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Update(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Category updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null | id == 0)
+            {
+                return NotFound();
+            }
+            Product productFromDb = _unitOfWork.Product.Get(u => u.Id == id);
+            if (productFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(productFromDb);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+            Product? obj = _unitOfWork.Product.Get(u => u.Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.Product.Remove(obj);
+            _unitOfWork.Save();
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToAction("Index");
+        }
+
+
+
+    }
+}
